a3fe3032397a8bd6debfe2c0c25dc681a9f503a5
John Stephen Entia <166895438+akoposidyan24@users.noreply.github.com>
2024-06-20 15:41:45 +0800

    Fix the edit functions
    Fix the issue when updating the previous record will be just copy.

--- includes/update.php
+++ includes/update.php
@@ -1,75 +1,34 @@
-<?php
-    include 'db.php';
+<?
 
-    // if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    //     $updatedData = json_decode(file_get_contents('php://input'), true);
-    //     $data = readCSV(false);
-    //     $headers = $data['headers'];
-    //     $rows = $data['data'];
+include 'db.php';
 
-    //     foreach ($rows as &$row) {
-    //         if ($row[0] == $updatedData['id']) { // Assuming 'id' is the first column
-    //             foreach ($headers as $index => $header) {
-    //                 if (isset($updatedData[$header])) {
-    //                     $row[$index] = $updatedData[$header];
-    //                 }
-    //             }
-    //             break;
-    //         }
-    //     }
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $updatedData = json_decode(file_get_contents('php://input'), true);
+    $data = readCSV(false);
+    $headers = $data['headers'];
+    $rows = $data['data'];
 
-    //     $file = fopen('../csvs/realtor_teams.csv', 'w');
-    //     fputcsv($file, $headers);
-    //     foreach ($rows as $row) {
-    //         fputcsv($file, $row);
-    //     }
-    //     fclose($file);
+    $file = fopen('../csvs/realtor_teams.csv', 'w'); 
 
-    //     echo json_encode(['status' => 'success', 'message' => 'Team member updated successfully']);
-    // }
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $updatedData = json_decode(file_get_contents('php://input'), true);
-    
-        // Log incoming data
-        error_log("Incoming data: " . print_r($updatedData, true));
-    
-        $data = readCSV(false);
-        $headers = $data['headers'];
-        $rows = $data['data'];
-    
-        // Log CSV data
-        error_log("CSV Headers: " . print_r($headers, true));
-        error_log("CSV Rows: " . print_r($rows, true));
-    
-        $idIndex = array_search('id', array_map('strtolower', $headers)); // Assuming 'id' is a header, using strtolower for case-insensitivity
-        if ($idIndex === false) {
-            echo json_encode(['status' => 'error', 'message' => 'ID column not found in CSV']);
-            exit();
-        }
-    
-        $isUpdated = false;
-        foreach ($rows as &$row) {
-            if ($row[$idIndex] == $updatedData['id']) {
-                foreach ($headers as $index => $header) {
-                    if (isset($updatedData[$header])) {
-                        $row[$index] = $updatedData[$header];
-                    }
+    fputcsv($file, $headers);
+
+    foreach ($rows as $row) {
+        if ($row[0] == $updatedData['id']) { 
+            foreach ($headers as $index => $header) {
+                if (isset($updatedData[$header])) {
+                    $row[$index] = $updatedData[$header];
+                } else {
+                    
+                    $row[$index] = '';
                 }
-                $isUpdated = true;
-                break;
             }
         }
-    
-        if ($isUpdated) {
-            $file = fopen('../csvs/realtor_teams.csv', 'w');
-            fputcsv($file, $headers);
-            foreach ($rows as $row) {
-                fputcsv($file, $row);
-            }
-            fclose($file);
-            echo json_encode(['status' => 'success', 'message' => 'Team member updated successfully']);
-        } else {
-            echo json_encode(['status' => 'error', 'message' => 'Record not found']);
-        }
+        fputcsv($file, $row); 
     }
+
+    fclose($file);
+
+    echo json_encode(['status' => 'success', 'message' => 'Team member updated successfully']);
+}
+?>
 ?>
